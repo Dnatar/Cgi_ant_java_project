bed61e9daf6a84687b059a1e083a876d24bf842b
Dinesh <dinesmail@gmail.com>
2017-10-26 17:05:56 +0530

    first commit

---
+++ src/com/cgi/business/GenerateToken.java
@@ -0,0 +1,19 @@
+package com.cgi.business;
+
+import java.util.Random;
+
+public class GenerateToken {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Generated Token=");
+		System.out.println(gentoken());
+
+	}
+
+	
+	private static double gentoken()
+	{
+		return new Random().nextGaussian();
+	}
+}
